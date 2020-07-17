7dc83aae275dc406a76dad6efd37cbf769123a83
yzimroni <yanay03@gmail.com>
2020-07-17 20:56:02 +0300

    Add light toggle

--- smartbulb.py
+++ smartbulb.py
@@ -5,9 +5,16 @@ secrets = {}
 with open("secrets.json") as f:
     secrets = json.load(f)
 
+# TODO add device details cache
 devices = lakeside.get_devices(secrets["username"], secrets["password"])
 d = devices[0]
+print("Bulb info: %s" % str(d))
 bulb = lakeside.bulb(d["address"], d["code"], d["type"])
 bulb.connect()
 
-print(bulb.get_status())
+if bulb.get_status().bulbinfo.packet.bulbstate.power == 0:
+    print("Toggling bulb on")
+    bulb.set_power(power=1)
+else:
+    print("Toggling bulb off")
+    bulb.set_power(power=0)
